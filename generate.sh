469cc5ab8d86f2c34c42c16e4c1bb52179ea806a
szatkus <szatkus@gmail.com>
2012-01-08 14:25:16 +0100

    ytyty

--- generate.sh
+++ generate.sh
@@ -3,7 +3,6 @@ SQL_CLIENT="sqlite3"
 DATABASE="9g.db"
 
 results=$($SQL_CLIENT $DATABASE "SELECT Id, Url, Title from '9gag' WHERE Checked='False' LIMIT $1")
-$SQL_CLIENT $DATABASE "UPDATE '9gag' SET Checked='True' WHERE Checked='False' LIMIT $1"
 
 echo '<?xml version="1.0" encoding="UTF-8"?>'
 echo '<?xml-stylesheet type="text/xsl" href="9gag.xsl"?>'
@@ -26,7 +25,7 @@ for gag in $results; do
 		esac
 		col=$(expr $col + 1)
 	done
-	
+	$SQL_CLIENT $DATABASE "UPDATE '9gag' SET Checked='True' WHERE Id=$id"
 	title=$(echo $title|sed "s/& /and /g")
 	echo "<title>$title</title>"
 	echo "<url>$url</url>"
